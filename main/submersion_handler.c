08b2da02411e4cacd05ebb3d7433fd15829a76a0
DmitriLyalikov <Dlyalikov01@manhattan.edu>
2024-01-21 19:52:08 -0500

    added initial skeleton for state

--- main/submersion_handler.c
+++ main/submersion_handler.c
@@ -1,4 +1,13 @@
+// Creator: Dmitri Lyalikov
+// Date: 2024.21.01
+// Purpose: Submersion event handler. Recieves MOB status and GPS coordinates. Sends SMS to the user.
+// over ESP-NOW and forwards over UART to UAV controller
+
+
+
 #include <stdio.h>
+
+
 
 void app_main(void)
 {
---
+++ main/submersion_handler.h
@@ -0,0 +1,11 @@
+#ifndef SUBMERSION_HANDLER_H_
+#define SUBMERSION_HANDLER_H_
+
+// Define MOB Status enum
+enum MOBStatus {
+  None,
+  Wake,
+  Ok,
+};
+
+// Define Submersion Handler class
